f24692e61a2dbeabdfe09b557b508aa202838037
liujh <liujh@gzsendi>
2021-07-31 16:39:38 +0800

    change

--- bin/control.bat
+++ bin/control.bat
@@ -2,5 +2,4 @@
 
 title _control.bat
 
-java -cp "./lib/*;./remotestcpserver_single.jar" cn.gzsendi.stcp.control.ControlClientStart -ssl false -token 123456 -trunnelHost 127.0.0.1 -trunnelPort 7000 -groups stcp1 -types sock5 -serverFrontPorts 10808
-
+java -cp "./lib/*;./remotestcpserver_single_sock5.jar" cn.gzsendi.stcp.control.ControlClientStart -ssl false -token gzsendi -trunnelHost 127.0.0.1 -trunnelPort 7000 -groups stcp1 -types tcp -serverFrontPorts 13306 -remoteHosts 127.0.0.1 -remotePorts 3306
--- bin/server.bat
+++ bin/server.bat
@@ -2,5 +2,5 @@
 
 title _stcpserver_single.bat
 
-java -cp "./lib/*;./remotestcpserver_single.jar" cn.gzsendi.stcp.server.StcpServerStart -ssl false -serverPort 7000 -token 123456
+java -cp "./lib/*;./remotestcpserver_single_sock5.jar" cn.gzsendi.stcp.server.StcpServerStart -ssl false -serverPort 7000 -token 123456
 
